d9f67fddf236c76e6e4935d8f7633266db690071
Muhammad Atta Othman Ahmed <muahann@gmail.com>
2019-11-13 15:18:19 +0200

    Update FCILuxorDjango.sh

--- FCILuxorDjango.sh
+++ FCILuxorDjango.sh
@@ -1,5 +1,5 @@
-sudo apt-get update
-sudo apt-get install git -y
+#sudo apt-get update
+#sudo apt-get install git -y
 wget -N https://download1519.mediafire.com/hqlix6b19bsg/we77oalq1ljtwtn/FCILuxorDjango.zip
 unzip FCILuxorDjango.zip
 cd WebProgDjango
