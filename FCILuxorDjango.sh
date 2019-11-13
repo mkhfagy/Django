c88e0ab895f926d5d8553229ef4ae299fefdd1a3
Muhammad Atta Othman Ahmed <muahann@gmail.com>
2019-11-13 13:55:50 +0200

    Fetch the project from web, install, run.
    ./FCILuxorDjango.sh

---
+++ FCILuxorDjango.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install git -y
+wget -N https://download1519.mediafire.com/hqlix6b19bsg/we77oalq1ljtwtn/FCILuxorDjango.zip
+unzip FCILuxorDjango.zip
+cd WebProgDjango
+python manage.py runserver
+
